a93a236ddf7b55832eca8c5168ffdc3f1f6c8593
parkerS123 <23pschwantes@athenian.org>
2020-09-12 20:03:00 +0000

    almost

--- my_code.py
+++ my_code.py
@@ -1,14 +1,27 @@
-# Collaborators (including web sites where you got help: (enter none if you didn't need help)
+# Collaborators (including web sites where you got help: (enter none if you didn't need help) = mom 
 #  
+answer = 1
 
-def factorial_calc(x):   #you may choose the name of the parameter
+def factorial_calc(number):   #you may choose the name of the parameter
 
-    return    # be sure to return the factorial
+    if number < 0:
+        print("There is no factorial for negative numbers")
+    elif number == 0:
+        print("Factorial of 0 is 1")
+    else: 
+        for x in range(1, number + 1):
+            answer == (x + 1) * 1
+            answer == answer * x
+        
+
+    return answer    # be sure to return the factorial
 
 
 if __name__ == '__main__':
     # Test your code with this first
     # Change the argument to try different values
+    #input("Input a number that you would like to know the factorial of... ")
+
     print(factorial_calc(5))
 
     # After you are satisfied with your results, use input() to prompt the user for a value:
